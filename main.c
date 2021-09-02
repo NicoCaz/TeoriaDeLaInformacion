211eb8b90a58e25f267439732ad70bafd91643fe
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2021-09-02 12:27:10 -0300

    Creo funcion 'leerArchivo'

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+void leeArchivo(char archFuente[100],int tamanioPalabra){
+    char palabra[tamanioPalabra];
+    FILE* arch = fopen(archFuente,"rt");
+    while(!feof(arch)){
+        
+    }
+}
+
 int main(int cantArgc, char *arg[]){
+
     return 0;
 }
+
+
