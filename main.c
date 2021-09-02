8e5c7ec81f7191603cc0ba0191512a5c5ed536e9
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2021-09-02 12:53:14 -0300

    lectura

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-void leeArchivo(char archFuente[100],int tamanioPalabra){
+void leeArchivo(char archFuente[],int tamanioPalabra){
     char palabra[tamanioPalabra];
     FILE* arch = fopen(archFuente,"rt");
     while(!feof(arch)){
-        fscanf(arch,"%s",palabra);
+        //fgets(auxline,200,arch)!=NULL
+        fgets(palabra,5,arch);
+        printf("%s \n",palabra);
     }
 }
 
 int main(int cantArgc, char *arg[]){
+    leeArchivo("anexo1.txt",5);
     return 0;
 }
 
