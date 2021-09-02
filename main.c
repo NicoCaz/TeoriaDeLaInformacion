77faba5187466e8653a82f96a2245b33259143f5
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2021-09-02 14:12:18 -0300

    a

--- main.c
+++ main.c
@@ -15,11 +15,16 @@ void leeArchivo(char archFuente[],int tamanioPalabra,TLista *L,int *cant);
 int anytoint(char *s, char **out);
 void cargalista(TLista *L, char dato[50]);
 void muestraLista(TLista L);
+void calculoCantInformacion(TLista L,int cant);
+void clear();
+
+
 int main(int cantArgc, char *arg[]){
+    system("cls");
     TLista L=NULL;
     int cant=0;
     leeArchivo("anexo1.txt",5,&L,&cant);
-    muestraLista(L);
+    calculoCantInformacion(L,cant);
     return 0;
 }
 
@@ -32,17 +37,6 @@ void leeArchivo(char archFuente[],int tamanioPalabra,TLista *L,int *cant){
         (*cant)++;
        // printf("%s \n",palabra);
     }
-}
-
-int anytoint(char *s, char **out) {
-    char *BASES = {"********@*#*****%"};
-    int base = 2;
-    char *bp = strchr(BASES, *s);
-    if (bp != NULL) {
-        base = bp - BASES;
-        ++s;
-    }
-    return strtol(s, out, base);
 }
 
 void cargalista(TLista *L, char dato[50]){
@@ -86,11 +80,43 @@ void muestraLista(TLista L){
     }
 }
 
-double logbase(double a, double base)
+float logbase(double a, double base)
 {
    return log(a) / log(base);
 }
 
 void calculoCantInformacion(TLista L,int cant){
+    float cantidadInfo;
+    float prob;
+    while(L!=NULL){
+        printf("palabra: %s cant: %d  \n",L->palabra,L->catidad);
+        prob=(L->catidad/(float)cant);
+        printf("%f \n",prob);
+        cantidadInfo=logbase(1/(float)prob,2);
+        //printf("la cantidad de informacion es de:%f \n",calculoCantInformacion);
+        L=L->sig;
+    }
+}
 
+
+int anytoint(char *s, char **out) {
+    char *BASES = {"********@*#*****%"};
+    int base = 2;
+    char *bp = strchr(BASES, *s);
+    if (bp != NULL) {
+        base = bp - BASES;
+        ++s;
+    }
+    return strtol(s, out, base);
+}
+
+
+void clear(){
+    #if defined(__linux__) || defined(__unix__) || defined(__Apple__)
+        system("clear");
+    #endif
+
+    #if defined(_WIN32) || defined(_WIN64)
+        system("cls");
+    #endif
 }
