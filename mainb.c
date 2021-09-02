e6c842a72785f21c6d9a45bc21cbe8d0fb03a355
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2021-09-02 19:40:54 -0300

    c

---
+++ mainb.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void leeArchivo(char archFuente[],float mat[][4]);
+void iniciaMat(float mat[][4]);
+void muestraMat(float mat[][4]);
+int main(){
+    float mat[4][4];
+    iniciaMat(mat);
+    leeArchivo("anexo1.txt",mat);
+    muestraMat(mat);
+    return 0;    
+}
+
+void iniciaMat(float mat[][4]){
+    for(int i=0;i<4;i++)
+        for(int j=0;j<4;j++)
+            mat[i][j]=0;
+}
+void muestraMat(float mat[][4]){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++)
+            printf("%d\t",mat[i][j]);
+        printf("\n");
+    }
+}
+void leeArchivo(char archFuente[],float mat[][4]){
+    char ant[3];
+    char act[3];
+    int fila,columna;
+    FILE* arch = fopen(archFuente,"rt"); 
+    strcpy(ant,"\0");
+    while(fgets(act,3,arch)!=NULL){
+        if(strcmp(ant,"\0")==0){
+            strcpy(ant,act);
+        }else{
+            fila=strtoul(ant,NULL ,2);
+            columna=strtoul(act,NULL ,2);
+            mat[fila][columna]=mat[fila][columna]+1;
+            strcpy(ant,act);
+        }
+    }
+
+}
+
