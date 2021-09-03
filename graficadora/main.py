3371716414789307f5c1fba17fe896379c5d47cb
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2021-09-03 00:24:12 -0300

    Ahora grafica el histograma

--- graficadora/main.py
+++ graficadora/main.py
@@ -1,8 +1,25 @@
-import numpy as numpy
+
 from io import open
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 
 arch=open("graficadora/salida.txt","r")
+fig, ax = plt.subplots()
 texto=arch.readlines()
-print(texto[1])
+simbolos=[]
+cantidad=[]
+informacion=[]
+entropia=[]
+numero=0
+for linea in texto:
+    linea=linea.split()
+    simbolos.append(numero)
+    cantidad.append(linea[1])
+    informacion.append(linea[2])
+    entropia.append(linea[3])
+    numero=numero+1
+
+ax.set_ylabel('Cantidad')
+ax.set_xlabel('simbolo')
+plt.bar(simbolos,cantidad)
+plt.show()
