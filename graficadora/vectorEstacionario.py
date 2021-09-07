9f307e8933b165f2c5ca7da0bc69e8f8d9259b28
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2021-09-07 13:47:22 -0300

    Creo vec estacionario (a chequear)

--- graficadora/vectorEstacionario.py
+++ graficadora/vectorEstacionario.py
@@ -1,12 +1,29 @@
 import numpy as np
+from numpy import ma
 
 
 arch=open("graficadora/matriz.txt","r")
 
 arch=arch.readlines()
 mat=[]
-for fila in arch:
+fila=[]
+for filas in arch:
+    filas=filas.split()
+    fila.append(float(filas[0]))
+    fila.append(float(filas[1]))
+    fila.append(float(filas[2]))
+    fila.append(float(filas[3]))
     mat.append(fila)
+    fila=[]
 
 mat=np.matrix(mat)
-print(mat)
+
+dimension=len(mat)
+mat=mat-np.identity(dimension)
+b=np.zeros(dimension)
+mat[-1]=np.ones(dimension)
+b[-1]=1
+vec=np.linalg.solve(mat,b)
+
+
+print(vec)
