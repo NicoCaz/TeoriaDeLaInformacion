98e43f5159b9089979889fd9c998bdb342a50fca
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2021-09-07 13:12:07 -0300

    correccion error

--- mainb.c
+++ mainb.c
@@ -23,9 +23,10 @@ void iniciaMat(float mat[][4]){
 void muestraMat(float mat[][4]){
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            printf("|\t%f\t",mat[i][j]);
+            printf("|\t%f\t",mat[j][i]);
         }   
-        printf("|\n-------------------------------------------------------------\n");
+        printf("\n"); 
+       // printf("|\n-------------------------------------------------------------\n");
     }
 }
 void leeArchivo(char archFuente[],float mat[][4]){
@@ -38,9 +39,9 @@ void leeArchivo(char archFuente[],float mat[][4]){
         if(strcmp(ant,"\0")==0){
             strcpy(ant,act);
         }else{
-            columna=strtoul(ant,NULL ,2);
-            fila=strtoul(act,NULL ,2);
-            mat[fila][columna]+=1;
+            fila=strtoul(ant,NULL ,2);
+            columna=strtoul(act,NULL ,2);
+            mat[columna][fila]+=1;
             strcpy(ant,act);
         }
     }
