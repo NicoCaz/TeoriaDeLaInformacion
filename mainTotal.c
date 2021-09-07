0236e475bb185bffca6ca943d5c0f19eefb9d620
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2021-09-07 12:53:08 -0300

    el ejecutor de todos los .exe

---
+++ mainTotal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int cantArgc, char *arg[]){
+    system("main.exe");
+    printf("\n------------------------\n");
+    system("mainb.exe");
+    system("graficadora\\main.exe");
+    return 0;
+    
+}
